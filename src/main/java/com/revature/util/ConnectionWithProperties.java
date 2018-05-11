b00db9b01d0963e2a1b894e0b364cd44a098525f
MichaelSikorski <mikesikorski2@gmail.com>
2018-05-11 19:36:55 -0400

    added missing import for ResourceBundle

--- src/main/java/com/revature/util/ConnectionWithProperties.java
+++ src/main/java/com/revature/util/ConnectionWithProperties.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class ConnectionWithProperties {
 
