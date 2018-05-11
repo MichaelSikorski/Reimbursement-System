fcf7b3089e8dca49999d0d922daf5576c2d7d8d7
MichaelSikorski <mikesikorski2@gmail.com>
2018-05-11 19:34:33 -0400

    replaced File with Bundle

--- src/main/java/com/revature/util/ConnectionWithProperties.java
+++ src/main/java/com/revature/util/ConnectionWithProperties.java
@@ -28,25 +28,29 @@ public class ConnectionWithProperties {
 	public static Connection getConnection() {
 		InputStream in = null;
 		try {
-			Properties props = new Properties();
+			//Properties props = new Properties();
 			//in = new FileInputStream("src/main/resources/db.properties");
 			//System.out.println(ConnectionWithProperties.class.getProtectionDomain().getCodeSource().getLocation().getFile());
-			in = new FileInputStream("C:\\Users\\Michael\\Documents\\workspace-sts-3.9.3.RELEASE\\Project_1\\src\\main\\resources\\db.properties");
-			props.load(in);
+			//in = new FileInputStream("src\\main\\resources\\db.properties");
+			//props.load(in);
+			ResourceBundle props = ResourceBundle.getBundle("db.properties");
 			return DriverManager.getConnection(
-					props.getProperty("jdbc.url"),
-					props.getProperty("jdbc.username"),
-					props.getProperty("jdbc.password"));
+					//props.getProperty("jdbc.url"),
+					//props.getProperty("jdbc.username"),
+					//props.getProperty("jdbc.password"));
+					props.getString("jdbc.url"),
+					props.getString("jdbc.username"),
+					props.getString("jdbc.password"));
 		} catch (SQLException e) {
 			System.err.println(e.getMessage());
 			System.err.println("SQL state: " + e.getSQLState());
 			System.err.println("Error code: " + e.getErrorCode());
-		} catch (FileNotFoundException fnfe) {
-			System.err.println(fnfe.getMessage());
-		} catch (IOException ioe) {
-			System.err.println(ioe.getMessage());
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+		//} catch (FileNotFoundException fnfe) {
+		//	System.err.println(fnfe.getMessage());
+		//} catch (IOException ioe) {
+		//	System.err.println(ioe.getMessage());
+		//} catch (Exception e) {
+		//	System.out.println(e.getMessage());
 		} finally {
 			try {
 				in.close();
