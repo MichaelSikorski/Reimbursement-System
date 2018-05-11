8ba58afe81a337b4a5fe8f08041dbe5f093abcc2
MichaelSikorski <mikesikorski2@gmail.com>
2018-05-11 18:39:27 -0400

    updated old references to project url

--- src/main/java/com/revature/servlets/RequestHelper.java
+++ src/main/java/com/revature/servlets/RequestHelper.java
@@ -9,35 +9,35 @@ public class RequestHelper {
 	public static Object process(HttpServletRequest req, HttpServletResponse res) {
 		String uri = req.getRequestURI();
 		switch (uri) {
-		case "/Project_1/loginEmployee.msp1":
+		case "/Reimbursement-System/loginEmployee.msp1":
 			System.out.println("Request helper received a request to login an employee");
 			return new LoginEmployeeService().userOnDb(req.getParameter("email"), req.getParameter("password"));
-		case "/Project_1/loginManager.msp1":
+		case "/Reimbursement-System/loginManager.msp1":
 			System.out.println("Request helper received a request to login a manager");
 			return new LoginManagerService().userOnDb(req.getParameter("email"), req.getParameter("password"));
-		case "/Project_1/getAllEmployees.msp1":
+		case "/Reimbursement-System/getAllEmployees.msp1":
 			System.out.println("Request helper received a request to get all employees");
 			return EmployeeService.getEmployeeService().getAllEmployees();
-		case "/Project_1/viewAllResolved.msp1":
+		case "/Reimbursement-System/viewAllResolved.msp1":
 			System.out.println("Request helper received a request to view all resolved requests");
 			return RequestService.getRequestService().getAllResolvedRequests();
-		case "/Project_1/viewAllPending.msp1":
+		case "/Reimbursement-System/viewAllPending.msp1":
 			System.out.println("Request helper received a request to view all pending requests");
 			return RequestService.getRequestService().getAllPendingRequests();
-		case "/Project_1/submitRequest.msp1":
+		case "/Reimbursement-System/submitRequest.msp1":
 			System.out.println("Request helper received a request to submit a request");
 			return RequestService.getRequestService().submitRequest(req.getParameter("requester"),
 					Integer.parseInt(req.getParameter("amount")),req.getParameter("purpose"));
-		case "/Project_1/employeeViewPending.msp1":
+		case "/Reimbursement-System/employeeViewPending.msp1":
 			System.out.println("Request helper received a request to view a specific employee's pending requests");
 			return RequestService.getRequestService().getEmployeesPendingRequests(req.getParameter("email"));
-		case "/Project_1/employeeViewPast.msp1":
+		case "/Reimbursement-System/employeeViewPast.msp1":
 			System.out.println("Request helper received a request to view a specific employee's past requests");
 			return RequestService.getRequestService().getEmployeesResolvedRequests(req.getParameter("email"));
-		case "/Project_1/employeeViewInfo.msp1":
+		case "/Reimbursement-System/employeeViewInfo.msp1":
 			System.out.println("Request helper received a request to view a specific employee's information");
 			return EmployeeService.getEmployeeService().getEmployee(req.getParameter("email"));
-		case "/Project_1/employeeUpdateInfo.msp1":
+		case "/Reimbursement-System/employeeUpdateInfo.msp1":
 			System.out.println("Request helper received a request to update an employee's information");
 			String email = req.getParameter("email");
 			String newUsername = req.getParameter("newUsername");
